192df629b1b3c211d8b58b80532af29982813a1b
dabao1955 <195328750@qq.com>
2023-05-09 03:08:25 +0800

    update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,9 +2,9 @@
 
 mkdir out
 
-BUILD_CROSS_COMPILE=/media/huaji/fc1e4abe-0fb6-45f0-a812-4410e3006a32/Lineage18.1/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-CLANG_PATH=/media/huaji/fc1e4abe-0fb6-45f0-a812-4410e3006a32/Lineage19.1/prebuilts/clang/host/linux-x86/clang-r383902/bin
-CROSS_COMPILE_ARM32=/media/huaji/fc1e4abe-0fb6-45f0-a812-4410e3006a32/Lineage18.1/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+BUILD_CROSS_COMPILE=$HOME/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CLANG_PATH=$HOME/clang/bin
+CROSS_COMPILE_ARM32=$HOME/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 CLANG_TRIPLE=aarch64-linux-gnu-
 
@@ -13,7 +13,7 @@ export PATH=${CLANG_PATH}:${PATH}
 
 make -j8 -C $(pwd) O=$(pwd)/out CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 \
     CC=clang \
-    vendor/mt6877_defconfig
+    vendor/full/r6_nodtb_defconfig
 
 A=$(date +%s)
 make -j8 -C $(pwd) O=$(pwd)/out CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 \
